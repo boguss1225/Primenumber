d24aeb0dff87940afecca53576135251c303483c
Aditya Pukale <adityapukale115@gmail.com>
2021-03-29 12:37:37 +1100

    Prime Number solution
    newly modified solution

--- primenumbersoln.py
+++ primenumbersoln.py
@@ -1,14 +1,22 @@
-print("This programe is for print Prime number (1,2,3,5,7,11,13,17...) of certain order\n please insert an number:")
-primenumber = [1,2,3,5,7]
-n = int(input())
+# enter the number 
+num=int(input("Enter the prime number: "))
+# checks if the number is greater than 1
+if num > 1:
+    for i in range (2,num):
+        #checks the number is prime or not
+        if(num % i) == 0:
+           print(num,"is not a prime number")
+           break
+        else:
+            print(num, "is prime number")
+            break
 
-if n > 1:
-    for i in range (2,n):
-      #  print(n, "is prime ")
-        while (n % i) == 0:
-           # print(n,"is not a prime number")
-            n += 1
-           # print(n)
-            
-    else:
-            print(n,"is prime number") 
+    while True:
+          num += 1
+          for i in range (2,num):
+             if num %i == 0:
+               break
+          else:
+            print(num," is a prime number")
+            break
+
